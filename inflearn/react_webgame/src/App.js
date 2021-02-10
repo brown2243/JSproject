a5f5060ebcc9acda47e7bbe71500e6175fa871b5
brown2243 <brown2243@naver.com>
2021-02-10 23:12:03 +0900

    02/10

--- inflearn/react_webgame/src/App.js
+++ inflearn/react_webgame/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Gugudan from "./component/Gugudan";
+import WordRelay from "./component/WordRelay";
 const app = () => {
   return (
     <div className="App">
       <Gugudan />
+      <WordRelay />
     </div>
   );
 };
