abfbb3dffab71dca43e5f5b4b00ae4c906a7aaea
brown Kim <brown2243@naver.com>
2021-06-17 00:24:43 +0900

    redux

--- 6.react.js/nomad_redux/react_redux/src/store.js
+++ 6.react.js/nomad_redux/react_redux/src/store.js
@@ -1,25 +1,41 @@
 import { createStore } from "redux";
+import { createAction } from "@reduxjs/toolkit";
 
-const ADD = "ADD";
-const DELETE = "DELETE";
+// const ADD = "ADD";
+// const DELETE = "DELETE";
 
-const addToDo = (text) => {
-  return {
-    type: ADD,
-    text,
-  };
-};
-const deleteToDo = (id) => {
-  return {
-    type: DELETE,
-    id,
-  };
-};
+// const addToDo = (text) => {
+//   return {
+//     type: ADD,
+//     text,
+//   };
+// };
+// const deleteToDo = (id) => {
+//   return {
+//     type: DELETE,
+//     id,
+//   };
+// };
+
+// const reducer = (state = [], action) => {
+//   switch (action.type) {
+//     case ADD:
+//       return [{ text: action.text, id: Date.now() }, ...state];
+//     case DELETE:
+//       return state.filter((toDo) => toDo.id !== action.id);
+//     default:
+//       return state;
+//   }
+// };
+
+const addToDo = createAction("ADD");
+const deleteToDo = createAction("DELETE");
+
 const reducer = (state = [], action) => {
   switch (action.type) {
-    case ADD:
+    case addToDo.type:
       return [{ text: action.text, id: Date.now() }, ...state];
-    case DELETE:
+    case deleteToDo.type:
       return state.filter((toDo) => toDo.id !== action.id);
     default:
       return state;
